3d0f82c005907beee24b6a28e2eb9d5743d48e77
Avrahm <avrahmk@me.com>
2021-11-28 07:47:42 -0500

    project 2 - section 1 - front end design

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import './App.css';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+  'https://media.giphy.com/media/htFUXJH5vjgIw/giphy.gif',
+  'https://media.giphy.com/media/l41m2CdrZUCtgtXWg/giphy.gif',
+  'https://media.giphy.com/media/TrXccx2cCI6Xu/giphy.gif',
+  'https://media.giphy.com/media/4dL4eV4pNb3MY/giphy.gif'
+]
 
 const App = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
   /*
   * This function holds the logic for deciding if a Phantom Wallet is
   * connected or not
@@ -48,9 +56,18 @@ const App = () => {
     }
   };
 
+  const sendGif = async (e) => {
+    e.preventDefault();
+
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
   /*
-   * When our component first mounts, let's check to see if we have a connected
-   * Phantom Wallet
+   * When our component first mounts, let's check to see if we have a connected Phantom Wallet
    */
   useEffect(() => {
     const onLoad = async () => {
@@ -60,6 +77,13 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIFs ... ')
+    }
+    setGifList(TEST_GIFS);
+  }, [walletAddress])
+
   return (
     <div className="App">
       <div className={walletAddress ? 'authed-container' : 'container'}>
@@ -68,13 +92,31 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {!walletAddress && (
+          {!walletAddress ? (
             <button
               className="cta-button connect-wallet-button"
               onClick={connectWallet}
             >
               Connect to Wallet
             </button>
+          ) : (
+            <div className="connected-container">
+              <form
+                onSubmit={sendGif}
+              >
+                <input type="text" value={inputValue}
+                  placeholder="Enter gif link!"
+                  onChange={e => setInputValue(e.target.value)} />
+                <button type="submit" className="cta-button submit-gif-button">Submit</button>
+              </form>
+              <div className="gif-grid">
+                {gifList.map(gif => (
+                  <div className="gif-item" key={gif}>
+                    <img src={gif} alt={gif} />
+                  </div>
+                ))}
+              </div>
+            </div>
           )}
         </div>
         <div className="footer-container">
